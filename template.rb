a0f073f4d811f26ca3bb08f164be77b1ee15f811
Wolfgang Barth <wob@swobspace.net>
2024-03-26 18:11:21 +0100

    first working version

---
+++ template.rb
@@ -0,0 +1,46 @@
+# rails application template
+run 'bundle remove tzinfo-data'
+gem 'simple_form'
+gem 'simple_form'
+gem 'wobapphelpers', git: 'https://github.com/swobspace/wobapphelpers', 
+                   branch: 'master'
+gem 'rails-i18n', '~> 7.0.0'
+gem 'font-awesome-sass', '~> 6.0'
+gem 'view_component'
+gem 'cancancan'
+
+run "bundle install"
+
+gem_group :test, :development do
+  gem 'rspec-rails'
+  gem 'dotenv'
+  # gem 'json_spec', require: false
+end
+
+gem_group :test do
+  gem "shoulda-matchers", require: false
+  gem 'factory_bot_rails'
+  gem "capybara"
+  gem 'selenium-webdriver'
+  gem 'webdriver'
+  gem 'launchy'
+end
+
+run "bundle install"
+
+inject_into_file 'config/application.rb', before: '  end' do
+  <<-CODE
+    config.generators do |g|
+      g.assets            false
+      g.helper            false
+      g.test_framework    nil
+      g.jbuilder          false
+    end
+  CODE
+end
+
+after_bundle do
+  # at last
+  # git add: ".", commit: %(-m 'checkin from rails application template')
+
+end
