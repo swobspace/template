cf7643a833e9ad77804da6347e791bda3c035964
Wolfgang Barth <wob@swobspace.net>
2024-03-29 13:48:05 +0100

    add more generators

--- README.md
+++ README.md
@@ -12,3 +12,14 @@ bin/rails app:template LOCATION=/path-to-template/template.rb
 ```
 
 See https://guides.rubyonrails.org/generators.html#application-templates
+
+~/.railsrc:
+```
+--database=postgresql
+--skip-test
+--skip-jbuilder
+--javascript esbuild
+--css bootstrap
+--template ~/Projects/github/template.rb
+
+```
--- template.rb
+++ template.rb
@@ -1,5 +1,7 @@
+#
 # rails application template
-# run 'bundle remove tzinfo-data'
+#
+run 'bundle remove tzinfo-data'
 append_to_file 'Gemfile' do
   <<~GEMS
 
@@ -37,7 +39,7 @@ inject_into_file 'config/application.rb', before: '  end' do
     config.generators do |g|
       g.assets            false
       g.helper            false
-      g.test_framework    nil
+      g.test_framework    :rspec
       g.jbuilder          false
     end
   CODE
@@ -49,7 +51,28 @@ end
 directory "~/Projects/github/template/template/docs", "docs"
 directory "~/Projects/github/template/template/docsrc", "docsrc"
 
+# add some npms
 run "yarn add --dev @antora/cli@3.1.0 @antora/site-generator@3.1.0" 
+run "yarn add bootstrap@5"
+run "yarn add @popperjs/core@2"
+
+run "yarn add jszip"
+run "yarn add pdfmake"
+run "yarn add datatables.net-bs5"
+run "yarn add datatables.net-buttons-bs5"
+run "yarn add datatables.net-responsive-bs5"
+
+# run generators
+run "bin/rails turbo:install"
+run "bin/rails turbo:install:redis"
+run "bin/rails css:install:bootstrap"
+run "bin/rails javascript:install:esbuild"
+
+generate "rspec:install"
+generate "simple_form:install --bootstrap --skip"
+
+generate "wobapphelpers:install"
+
 
 after_bundle do
   # at last
