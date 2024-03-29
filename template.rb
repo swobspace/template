55ccbca3d06e253de338dc6510106c1bfdfb2806
Wolfgang Barth <wob@swobspace.net>
2024-03-29 11:52:41 +0100

    avoid gem duplication on rerun bin/rails app:template
      the ruby generators 'gem' and 'gem_group' doesn't detect
      always inserted gem entries. Using insert works better

--- template.rb
+++ template.rb
@@ -1,29 +1,33 @@
 # rails application template
-run 'bundle remove tzinfo-data'
-gem 'simple_form'
-gem 'simple_form'
-gem 'wobapphelpers', git: 'https://github.com/swobspace/wobapphelpers', 
-                   branch: 'master'
-gem 'rails-i18n', '~> 7.0.0'
-gem 'font-awesome-sass', '~> 6.0'
-gem 'view_component'
-gem 'cancancan'
+# run 'bundle remove tzinfo-data'
+append_to_file 'Gemfile' do
+  <<~GEMS
 
-run "bundle install"
+  # --- TEMPLATE START ---
+  gem 'simple_form'
+  gem 'wobapphelpers', git: 'https://github.com/swobspace/wobapphelpers', 
+                     branch: 'master'
+  gem 'rails-i18n', '~> 7.0.0'
+  gem 'font-awesome-sass', '~> 6.0'
+  gem 'view_component'
+  gem 'cancancan'
 
-gem_group :test, :development do
-  gem 'rspec-rails'
-  gem 'dotenv'
-  # gem 'json_spec', require: false
-end
+  group :test, :development do
+    gem 'rspec-rails'
+    gem 'dotenv'
+    # gem 'json_spec', require: false
+  end
 
-gem_group :test do
-  gem "shoulda-matchers", require: false
-  gem 'factory_bot_rails'
-  gem "capybara"
-  gem 'selenium-webdriver'
-  gem 'webdriver'
-  gem 'launchy'
+  group :test do
+    gem "shoulda-matchers", require: false
+    gem 'factory_bot_rails'
+    gem "capybara"
+    gem 'selenium-webdriver'
+    gem 'webdriver'
+    gem 'launchy'
+  end
+  # --- TEMPLATE END ---
+  GEMS
 end
 
 run "bundle install"
@@ -38,6 +42,14 @@ inject_into_file 'config/application.rb', before: '  end' do
     end
   CODE
 end
+
+#
+# create antora doc tree
+#
+directory "~/Projects/github/template/template/docs", "docs"
+directory "~/Projects/github/template/template/docsrc", "docsrc"
+
+run "yarn add --dev @antora/cli@3.1.0 @antora/site-generator@3.1.0" 
 
 after_bundle do
   # at last
